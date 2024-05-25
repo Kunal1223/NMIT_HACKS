58fe1c7db48a81934c393de981a42a006c176a04
Kunal Kishor <kunalbth2002@gmail.com>
2024-05-25 13:11:44 +0530

    creating ngo , restro and user plateform

---
+++ client/src/Components/Restaurant/Restaurant.js
@@ -0,0 +1,65 @@
+import React, { useContext, useEffect } from 'react'
+import CardN from './CardN'
+import NGOContext from '../context/NGO/NGOContext';
+import { useNavigate } from 'react-router-dom'
+import LoginerrorAni from '../../assets/loginfirst.json'
+import Lottie from 'lottie-react';
+
+const Restaurant = () => {
+  let Navigate = useNavigate();
+
+  const context = useContext(NGOContext);
+  const { NGO, getNGO } = context;
+  const email = localStorage.getItem('email');
+  // console.log(NGO)
+ 
+  useEffect(() => {
+    getNGO(email);
+  }, [NGO, getNGO]);
+
+  const returback = (e) => {
+    e.preventDefault();
+    Navigate('/');
+  }
+
+  const movePacket = (e) => {
+    e.preventDefault();
+    Navigate('/packets');
+  }
+
+  return (
+    <>
+      {(!localStorage.getItem('restroAuthToken')) && (!localStorage.getItem('ngoAuthToken'))
+        ?
+        <div className='mt-16'>
+          <Lottie animationData={LoginerrorAni} className='animation h-72' />
+          <div className="flex justify-center">
+            <h1 className='text-red-600 font-bold text-4xl ' >Please Login First</h1>
+            <button className='ml-4 text-xl bg-green-600 text-white cursor-pointer 
+             px-6 rounded-lg' onClick={returback}>Home</button>
+          </div>
+        </div>
+        :
+        <>
+          <button className='bg-green-500 text-white absolute right-10 my-6 py-2 px-2
+          rounded-lg cursor-pointer' onClick={movePacket}>Add Packets
+          </button>
+          <div className='mt-10 grid grid-cols-1 gap-x-4 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8 cursor-pointer'>
+            {
+              Array.isArray(NGO) && NGO.length > 0 ? (
+                NGO.map((ngo) => {
+                  return (
+                      <CardN ngo_detail={ngo} />
+                  );
+                })
+              ) : (
+                <p>No ngos available</p>
+              )}
+          </div>
+        </>
+      }
+    </>
+  )
+}
+
+export default Restaurant
---
+++ server/routes/authU.js
@@ -0,0 +1,108 @@
+const express = require("express");
+const { body, validationResult } = require("express-validator");
+const User = require("../models/User");
+const router = express.Router();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+// const fetchuser = require("../middleware/fetchuser");
+
+router.post(
+  "/createUser",
+  [
+    body("name", "Enter the name of at least 3 characters").isLength({
+      min: 3,
+    }),
+    body("email", "Enter the valid email").isEmail(),
+    body("password", "Enter the password of at least 5 characters").isLength({
+      min: 5,
+    })
+  ],
+  async (req, res) => {
+
+    try {
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ success: false, errors: errors.array(), message: "Please Enter the valid data" });
+      }
+
+      const salt = await bcrypt.genSalt(10);
+      const secpass = await bcrypt.hash(req.body.password, salt);
+
+      let user = await User.findOne({ email: req.body.email });
+
+      if (user) {
+        return res.status(400).json({ success: false, message: "Sorry user already exists" });
+      } else {
+        user = await User.create({
+          name: req.body.name,
+          email: req.body.email,
+          password: secpass,
+          phone: req.body.phone,
+          location : req.body.location,
+        });
+      }
+
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+
+    const authtoken = jwt.sign(data, process.env.JWT_SECRET);
+    res.status(201).json({ success: true, authtoken , message:"Resiter Successfully" });
+
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ success: false, message: "Server error" });
+    }
+  }
+);
+
+router.post( 
+  "/loginUser",
+  [
+    body("email", "Enter the valid email").isEmail(),
+    body("password", "Enter a correct pass").exists(),
+  ],
+
+  async (req, res) => {
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ success:false, errors: errors.array(), message:"Email or Password not Match" });
+    }
+
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ success:false, message: "Email or Password not Match" });
+      }
+
+      const passwordCompare = await bcrypt.compare(password, user.password);
+      if (!passwordCompare) {
+        return res.status(400).json({ success:false, error: "Email or Password not Match" });
+      }
+
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      const authtoken = jwt.sign(data, process.env.JWT_SECRET);
+      res.status(201).json({ success:true, authtoken , message:"Login Successfully" });
+
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ success:false, message: "Server error" });
+    }
+  }
+);
+
+module.exports = router;
